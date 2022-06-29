256e0433cb8cdc145ae3ee148775f83af345e03e
root <root@DESKTOP-Q5LA3O5.localdomain>
2022-06-29 17:17:56 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+const path = require('path');           
+const PORT = process.env.PORT || 5000; 
+
+app.use(cors());
+app.use(bodyParser.json());
+
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+client.connect();
+
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
+});
